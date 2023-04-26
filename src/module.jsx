1f4e1c7e70c137266fd883a68b58802d75878dad
Serik <askaroff_serik@mail.ru>
2023-04-26 09:42:11 +0300

    first commit

---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import serik from './img/LEPP0577.jpg';
+
+const Module = () => (
+  <>
+    <h1>Hi, React</h1>
+    <img width={500} src={serik} />
+  </>
+);
+
+export default Module;
